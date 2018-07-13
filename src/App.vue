d608bd86f4b1dd0a92ee6cefd7b2352fdf61d17d
Taspyn <taspyn.akirachix@gmail.com>
2018-07-13 17:31:47 +0300

    my news app code

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
-    <form>
-    	<textarea placeholder="add new task" v-model="newTask"></textarea>
-    	<p>{{newTask}}</p>
-    	<button type="button" v-on:click="addTask">Add Task{{tasks.length}}</button>
-    </form>
-    <ul> <li v-for="task in tasks ">{{task}}</li>
+   <div class="content" v-for="article in news">
+    <img :src="article.urlToImage" alt="">
+    <h3>
+      {{article.title}}
+    </h3>
+    <h4>
+      {{article.author}}
+    </h4>
+    <blockquote>
+    <p>{{article.description}}</p>
 
-    </ul>
+    </blockquote>
+    <a href="#" target="blank" class="button">Read More</a>
+  </div>
+
 
   </div>
 </template>
 
 <script>
+
+const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=631a7de19d75427ea6afea262d7e9f6b";
+import axios from 'axios'
+
+const data = {
+  news:[]
+}
+
+
 export default {
   data: function() {
-    return {
-      title: 'My Awesome App',
-      newTask: 'This is a Sample',
-     
-      tasks:[
-
-      ]
-    }
+    return data 
+  },
+  created(){
+  this.loadNews();
   },
   methods: {
-  	addTask(){
-  	
-  		this.tasks.unshift(this.newTask);
-  		this.newTask = '';
-  		this.tasks.length;
-
-  	}
+  	   loadNews(){
+      axios.get(url).then(function(response) {
+        console.log(response.data.articles)
+        data.news = (response.data.articles)
+     }).catch(function(error) {
+        console.log(error.message)
+    })
+   }
   }
 
 }
